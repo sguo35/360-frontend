cc5da8e17d194fd5e962862f0e94f744a3e7e082
Jaiveer Singh <j.singh@berkeley.edu>
2018-10-14 14:26:53 -0700

    Wow setting up draggable starts now

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -9,7 +9,7 @@ class HelloWorld extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            bob <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
