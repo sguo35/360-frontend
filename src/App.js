6b0b9fe29196ec9b13de16ad4b608576aef21576
Your Name <sguo35@gmail.com>
2018-10-18 22:41:16 -0700

    feat: add basic redux boilerplate

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import logo from './logo.svg';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HelloWorld from './HelloWorld';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={HelloWorld} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={HelloWorld} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import createStore from 'redux';
+import rootReducer from './reducers/rootReducer';
+
+export const store = createStore(rootReducer, {
+    loginToken: ""
+});
