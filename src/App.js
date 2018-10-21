ccb01bdd9e5f7c4a2d4bd210a1be7a725a24ee77
Shiyuan Guo <sguo35@gmail.com>
2018-10-21 01:06:34 -0700

    feat: add select project page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HelloWorld from './HelloWorld';
 import Rate from './components/Rate/Rate';
+import SelectProject from './components/SelectProject/SelectProject';
 
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+import { store } from './redux/store';
 
 import 'antd/dist/antd.css';
 class App extends Component {
@@ -14,7 +15,8 @@ class App extends Component {
       <Provider store={store}>
         <BrowserRouter>
           <Switch>
-            <Route path="/" component={Rate} />
+            <Route exact path="/" component={Rate} />
+            <Route path="/selectProject" component={SelectProject} />
           </Switch>
         </BrowserRouter>
       </Provider>
---
+++ src/components/SelectProject/SelectProjectCenter.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import { Menu, Icon, Button } from 'antd';
+
+export default class SelectProjectCenter extends React.Component {
+  onLogoClick = () => {
+    alert("Go to dashboard")
+  }
+
+  render() {
+    return (
+      <div className="Rate-left-container">
+        <Menu
+          onClick={() => { }}
+          mode='vertical'
+          theme='dark'
+          style={{
+            width: 250,
+            height: '100%'
+          }}
+        >
+          <Icon type='redo' style={{
+            fontSize: 36,
+            marginTop: '35%',
+            marginLeft: '41%',
+            marginRight: '50%',
+            marginBottom: 30,
+            cursor: 'pointer'
+          }} onClick={this.onLogoClick} />
+          <p style={{
+            textAlign: 'center',
+            marginBottom: '35%',
+            fontSize: 13,
+            letterSpacing: '10px',
+            cursor: 'pointer'
+          }} onClick={this.onLogoClick}>360FACTORS</p>
+
+          <Menu.Item key='1' style={{
+            height: '10%',
+            display: 'flex',
+            justifyContent: 'flex-start',
+            padding: 15
+          }}>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column'
+            }}><Icon type='file-text' theme='outlined' style={{
+              fontSize: 32,
+              marginLeft: 12
+            }} />
+            </div>
+
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column'
+            }}>
+              <p style={{
+                margin: 20,
+                marginTop: 15
+              }}>Attendy</p>
+            </div>
+          </Menu.Item>
+
+          <Menu.Item key='2' style={{
+            height: '10%',
+            display: 'flex',
+            justifyContent: 'flex-start',
+            padding: 15
+          }}>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column'
+            }}><Icon type='file-text' theme='outlined' style={{
+              fontSize: 32,
+              marginLeft: 12
+            }} />
+            </div>
+
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column'
+            }}>
+              <p style={{
+                margin: 20,
+                marginTop: 15
+              }}>360</p>
+            </div>
+          </Menu.Item>
+
+          <Menu.Item key='3' style={{
+            height: '10%',
+            display: 'flex',
+            justifyContent: 'flex-start',
+            padding: 15
+          }}>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column'
+            }}><Icon type='file-text' theme='outlined' style={{
+              fontSize: 32,
+              marginLeft: 12
+            }} />
+            </div>
+
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column'
+            }}>
+              <p style={{
+                margin: 20,
+                marginTop: 15
+              }}>Events</p>
+            </div>
+          </Menu.Item>
+
+          <Menu.Item key='4' style={{
+            height: '10%',
+            display: 'flex',
+            justifyContent: 'flex-start',
+            padding: 15
+          }}>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column'
+            }}><Icon type='file-text' theme='outlined' style={{
+              fontSize: 32,
+              marginLeft: 12
+            }} />
+            </div>
+
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column'
+            }}>
+              <p style={{
+                margin: 20,
+                marginTop: 15
+              }}>Final Project</p>
+            </div>
+          </Menu.Item>
+        </Menu>
+      </div>
+    )
+  }
+}
+
