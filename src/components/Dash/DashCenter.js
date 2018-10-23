bb9466909014eff55aefe6bf1d842284d065659d
Jaiveer Singh <j.singh@berkeley.edu>
2018-10-23 10:09:51 -0700

    just got to fix the graph heights in dash

--- src/components/Dash/DashCenter.js
+++ src/components/Dash/DashCenter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Card, Tag, Button } from 'antd';
+import { Card, Tag, Button, Icon, Row, Col } from 'antd';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import { connect } from 'react-redux';
 import { store } from '../../redux/store';
@@ -14,8 +15,7 @@ export default class DashCenter extends React.Component {
   }
   render = () => {
     let cardStyle = {
-      alignItems: 'center',
-      margin: 20
+      alignItems: 'center'
     };
     let text = "Gwynevere actively communicates outside of emailings, and is easy to get ahold of. Gwynevere is always ready to email and will take simple off of his schedule and this clean our cloud in terms of cloud transparent and getting stuff done. Gwynevere offers great insight and perspective to cloud goals and discussions such as when he/she proposed to angle our project towards an easier implementation for the user, moving the forward. Gwynevere stepped out of their comfort zone by taking on the challenge of AngularJS, efficiently learning and transparenting on a new skill even though it may have been new or hard for them.Gwynevere’s engagement of transparent is outstanding because he/she takes whatever amount of simple to create a engagement product .The execution of the website created as well as the Attendy app and its functions shows extraordinary effort and thought, and it is delivered in a simple improvement. Gwynevere consistently  sets up email and listens to others, which is something not many others are willing to take on or do. It is beneficial to the because it propels the cloud forward because it builds a sense of trust. Gwynevere is always looking to push himself technically and this tendency to create and develop in this improvement clean our progress/cloud dynamic by expanding what our cloud is capable of accomplishing and what we can incorporate.";
     var personLeadership = .6;
@@ -33,30 +33,50 @@ export default class DashCenter extends React.Component {
     allProductivity = Math.sqrt(allProductivity);
 
     return (
+      <Scrollbars style={{
+        width: '100%',
+        height: '100%'
+      }}>
       <div className="Dash-center-pane">
-        <div className="Dash-header">
-          <Card>
-            <h1 style={{fontSize:50, textAlign: 'center', margin: 0}}>
-              Welcome <span style={{color:'#096dd9'}}>Gwynevere</span>
-            </h1>
-          </Card>
-        </div>
-        <div className="Dash-graph-row">
-          <Card style={cardStyle}>
-            <Wordcloud text={text}></Wordcloud>
-          </Card>
-          <Card style={cardStyle}>
-            <Radar
-              myLeadership={personLeadership}
-              myEngagement={personEngagement}
-              myProductivity={personProductivity}
-              teamLeadership={allLeadership}
-              teamEngagement={allEngagement}
-              teamProductivity={allProductivity}
-            ></Radar>
-          </Card>
-        </div>
+        <Row gutter={16} type="flex" justify="center" style={{marginBottom : 16}}>
+          <Col span={20}>
+            <Card>
+              <h1 style={{fontSize:50, textAlign: 'center', margin: 0}}>
+                Welcome <span style={{color:'#096dd9'}}>Gwynevere</span>
+              </h1>
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={16} type="flex" justify="center" style={{marginBottom : 16}}>
+          <Col span={10}>
+            <Card style={cardStyle}>
+              <Wordcloud text={text}></Wordcloud>
+            </Card>
+          </Col>
+          <Col span={10}>
+            <Card style={cardStyle}>
+              <Radar
+                myLeadership={personLeadership}
+                myEngagement={personEngagement}
+                myProductivity={personProductivity}
+                teamLeadership={allLeadership}
+                teamEngagement={allEngagement}
+                teamProductivity={allProductivity}
+              ></Radar>
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={16} type="flex" justify="center" style={{marginBottom : 16}}>
+          <Col span={20}>
+            <Card>
+              <h1 style={{fontSize:30, textAlign: 'center', margin: 0, height:1000}}>
+                Comments here
+              </h1>
+            </Card>
+          </Col>
+        </Row>
       </div>
+      </Scrollbars>
     )
   }
 }
