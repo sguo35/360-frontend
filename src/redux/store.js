2185d8b20bcb7a622e23f034589deb5b4de25449
Your Name <sguo35@gmail.com>
2018-10-23 22:19:55 -0700

    fix: permanent persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import { rootReducer } from './reducers/rootReducer';
 
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
 
 const persistConfig = {
   key: 'root',
   storage,
+  stateReconciler: hardSet
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
