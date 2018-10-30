f9a0260446bfee45de644461716fca2488f02917
Jaiveer Singh <j.singh@berkeley.edu>
2018-10-30 00:31:10 -0700

    was testing sheets stuff but don't need it

---
+++ src/sheetsTest.js
@@ -0,0 +1,14 @@
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('./client_secret.json');
+
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+var doc = new GoogleSpreadsheet('*spreadsheet ID*');
+
+// Authenticate with the Google Spreadsheets API.
+doc.useServiceAccountAuth(creds, function (err) {
+
+  // Get all of the rows from the spreadsheet.
+  doc.getRows(1, function (err, rows) {
+    console.log(rows);
+  });
+});
